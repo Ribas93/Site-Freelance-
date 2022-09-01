35545d893c245063ad9d76137eb5d1d5ff31aa82
Ribas93 <ribasoficial01@gmail.com>
2022-08-31 23:20:50 -0300

    script para validacao do telefone e criacao de um array com os clientes

---
+++ funcoes.js
@@ -0,0 +1,34 @@
+
+        let botao = document.querySelector('#botao2')
+        
+        const clientes = []
+
+        botao.addEventListener('click',function (){
+            
+            let regex = new RegExp('^((1[1-9])|([2-9][0-9]))((3[0-9]{3}[0-9]{4})|(9[0-9]{3}[0-9]{5}))$');
+
+            let res = document.querySelector('#res')
+            let cel = document.querySelector('#celular').value
+            let nome = document.querySelector('#nome').value
+
+
+            
+             if(regex.test(cel))
+             {  
+                 res.style.color = "#10AC84"
+                 res.innerHTML = `Ola ${nome}, seu registro foi cadastrado com sucesso. Entraremos em contato em breve! </p>`
+                 clientes.push({contato: nome, telefone:cel})
+             }
+            else
+            {
+                res.style.color = "#EE5253"
+                res.innerHTML = `Telefone ${cel} inválido!`
+            }   
+                  
+        })
+        
+    
+
+
+     
+        
